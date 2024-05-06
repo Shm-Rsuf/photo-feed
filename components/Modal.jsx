084f77ad12f1fc4f07f17308437ed37a4c2f94e4
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-05-06 09:59:16 +0600

    modal and intercepting routing added & modal is working

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,24 +1,26 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 const Modal = ({ children }) => {
   const modalRef = useRef(null);
+  const router = useRouter();
 
   useEffect(() => {
     modalRef?.current.showModal();
   }, []);
 
   /* hideModal function */
-  const hideModal = () => {
-    console.log("hide");
+  const onHide = () => {
+    router.back();
   };
 
   return createPortal(
     <dialog
       ref={modalRef}
-      onClose={hideModal}
+      onClose={onHide}
       className='shadow-teal-700 shadow-md border border-teal-600 flex flex-col p-2 rounded-md dark:bg-black dark:bg-opacity-95 dark:text-gray-100'
     >
       <span onClick={onHide} className='flex justify-end cursor-pointer'>
